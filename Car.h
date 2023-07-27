11b586030e9f13b8c2a111233bee907ad5952048
rxm3n <41157757+RayyuShao@users.noreply.github.com>
2023-07-26 21:10:36 -0400

    Defined class Car Defined numDoors for number of doors Defined a constructor and getters and setters Defined displayInfo

--- Car.h
+++ Car.h
@@ -1,1 +1,23 @@
+// Raymond Shao
+// CIS 1202 800
+// July 26, 2023
+
 #pragma once
+
+#ifndef CAR_H
+#define CAR_H
+
+#include "Vehicle.h"
+
+class Car {
+private: 
+	int numDoors;
+public: 
+    Car();
+    Car(string manufacturer, int yearBuilt, int numDoors);
+    int getNumDoors() const;
+    void setNumDoors(int numDoors);
+    void displayInfo() const;
+};
+
+#endif
